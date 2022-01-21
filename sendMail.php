e964154adf4f3581f0a764dad9b4f84d9d8ef7ee
hritik <hritikmiddha10@gmail.com>
2022-01-22 01:24:10 +0530

    werdfg

--- sendMail.php
+++ sendMail.php
@@ -1,38 +1,56 @@
+<?php	
+session_start();
+include '_dbConnect.php';
+require("vendor/autoload.php");
+require_once("php-mailer/PHPMailer.php");
+require_once("php-mailer/SMTP.php");
+require_once("php-mailer/Exception.php");
+$email = $_SESSION['email'];
+use PHPMailer\PHPMailer\PHPMailer;
+?>
   <?php
-	session_start();
-	include '_dbConnect.php';
-	require("vendor/autoload.php");
-	require_once("php-mailer/PHPMailer.php");
-	require_once("php-mailer/SMTP.php");
-	require_once("php-mailer/Exception.php");
-	use PHPMailer\PHPMailer\PHPMailer;
-	require 'vendor/autoload.php';
-	?>
-  <?php
-	$email = $_SESSION['email'];
-	$phpmailer = new PHPMailer(true);
-	$phpmailer->isSMTP();
-	$phpmailer->SMTPAuth = true;
-	$phpmailer->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-	$phpmailer->Host = "smtp.gmail.com";
-	$phpmailer->Port = "587";
-	$phpmailer->Username = "testmailassignmentphp@gmail.com";
-	$phpmailer->Password = "Hritik@123!!";
-	$phpmailer->setFrom("testmailassignmentphp@gmail.com");
-	$phpmailer->addAddress($email);
-	$phpmailer->isHTML(true);
-	$phpmailer->Subject = "Verify email";
-	$phpmailer->Body    = "You will be subscribed to XKCD challenge after verifying!
-		https://testheroku1088.herokuapp.com/welcome.php?token=$token\n";
-	if ($phpmailer->send()) {
-		echo '<br> <br><br> <div class="alert container alert-success alert-dismissible fade show" role="alert">
-			<strong>Email verification sent!!!</strong>  Please verify your email address.
-		  </div>';
-	} else {
-		echo "{$phpmailer->ErrorInfo}";
-	}
+	$mail = new PHPMailer(true);
+		$email = $_SESSION['email'];
+		function getComic() {
+			$rand_comic = rand(0,1000);
+			$url    = 'http://xkcd.com/' . $rand_comic . '/info.0.json';
+			$result = json_decode( file_get_contents( $url ), true );
+			return $result;
+		}
+		$comic = getComic();
+		$title = 'Your New Comic : ' . $comic['safe_title'];
+		$name = $response_data->title;
+		$subject = "$response_data->title";
+		$urlun = "https://testheroku1088.herokuapp.com/unsubscribe.php?email=$email";
 
-
+		$mail->isSMTP();
+		$mail->SMTPAuth = true;
+		$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+		$mail->Host = "smtp.gmail.com";
+		$mail->Port = "587";
+		$mail->Username = "testmailassignmentphp@gmail.com";
+		$mail->Password = "Hritik@123!";
+		$mail->setFrom("testmailassignmentphp@gmail.com");
+		$mail->addAddress("hritikmiddha10@gmail.com");
+		$mail->isHTML(true);
+		$mail->Subject = "Mailer To Go Test";
+		$mail->Body = '
+  	          <p>Hello XKCDian</p>
+  	          Here is your new comic.
+  	          <h3>' . $comic['safe_title'] . "</h3>
+  	          <img src='" . $comic[' img'] . "' alt='some comic hehe'/>
+			<br />
+			To read the comic,  --> <a target='_blank' href='https://xkcd.com/" . $comic[' num'] . "'>Click here</a><br /> 
+			To Unsubscribe the Xkcd,  --> <a target='_blank' href='" .$urlun."'>Click here</a><br />";
+		$mail->addStringAttachment(file_get_contents($url), "$subject");
+		if ($mail->send()) {
+			echo '<br> <br><br> <div class="alert container alert-success alert-dismissible fade show" role="alert">
+				<strong>Email verification sent!!!</strong>  Please verify your email address.
+			  </div>';
+		} else {
+			echo "{$mail->ErrorInfo}";
+		}
+		header("Location: success.php");
 	?>
 
 
