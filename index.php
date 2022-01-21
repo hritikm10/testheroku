d24976beba19931519c6ccbbe8d02779d45009c0
hritik <hritikmiddha10@gmail.com>
2022-01-21 18:46:15 +0530

    wedfg

--- index.php
+++ index.php
@@ -1,5 +1,41 @@
 <?php
   echo "Hritik";
+require("vendor/autoload.php");
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
+// Passing true enables exceptions.
+$phpmailer = new PHPMailer(true);
+
+try {
+  // Configure SMTP
+  $phpmailer->isSMTP();
+  $phpmailer->SMTPAuth = true;
+  $phpmailer->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+
+  // ENV Credentials
+  $phpmailer->Host = "smtp.gmail.com";
+  $phpmailer->Port = "587";
+  $phpmailer->Username = "testmailassignmentphp@gmail.com";
+  $phpmailer->Password = "Hritik@123!";
+//   $mailertogo_domain = getenv("MAILERTOGO_DOMAIN", true);
+
+  // Mail Headers
+  $phpmailer->setFrom("testmailassignmentphp@gmail.com");
+  // Change to recipient email. Make sure to use a real email address in your tests to avoid hard bounces and protect your reputation as a sender.
+  $phpmailer->addAddress("hritikmiddha10@gmail.com");
+
+  // Message
+  $phpmailer->isHTML(true);
+  $phpmailer->Subject = "Mailer To Go Test";
+  $phpmailer->Body    = "<b>Hi</b>\nTest from Mailer To Go 😊\n";
+  $phpmailer->AltBody = "Hi!\nTest from Mailer To Go 😊\n";
+
+  // Send the Email
+  $phpmailer->send();
+  echo "Message has been sent";
+} catch (Exception $e) {
+  echo "Message could not be sent. Mailer Error: {$phpmailer->ErrorInfo}";
+}
 
 ?>
