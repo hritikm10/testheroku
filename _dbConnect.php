ef26e4c9e0b3ee6def5301b87f82ceba3855b522
hritik <hritikmiddha10@gmail.com>
2022-01-21 23:49:32 +0530

    werdfg

--- _dbConnect.php
+++ _dbConnect.php
@@ -1,8 +1,12 @@
 <?php 
-    $servername = "us-cdbr-east-05.cleardb.net";
-    $username = "bbb1b74a1cdfb2";
-    $password = "675d2913";
-    $database = "heroku_b0ca9f11c1fddad";
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "xkcddb";
+    // $servername = "us-cdbr-east-05.cleardb.net";
+    // $username = "bbb1b74a1cdfb2";
+    // $password = "675d2913";
+    // $database = "heroku_b0ca9f11c1fddad";
     $conn = mysqli_connect($servername,$username,$password,$database);
     if(!$conn)
     {
