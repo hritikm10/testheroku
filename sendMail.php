24470aef47018f9d731424bf9d0f799490eb3d97
hritik <hritikmiddha10@gmail.com>
2022-01-22 01:28:13 +0530

    werdfg

--- sendMail.php
+++ sendMail.php
@@ -1,4 +1,4 @@
-<?php	
+<?php
 session_start();
 include '_dbConnect.php';
 require("vendor/autoload.php");
@@ -6,51 +6,20 @@ require_once("php-mailer/PHPMailer.php");
 require_once("php-mailer/SMTP.php");
 require_once("php-mailer/Exception.php");
 $email = $_SESSION['email'];
+
 use PHPMailer\PHPMailer\PHPMailer;
 ?>
   <?php
-	$mail = new PHPMailer(true);
-		$email = $_SESSION['email'];
-		function getComic() {
-			$rand_comic = rand(0,1000);
-			$url    = 'http://xkcd.com/' . $rand_comic . '/info.0.json';
-			$result = json_decode( file_get_contents( $url ), true );
-			return $result;
-		}
-		$comic = getComic();
-		$title = 'Your New Comic : ' . $comic['safe_title'];
-		$name = $response_data->title;
-		$subject = "$response_data->title";
-		$urlun = "https://testheroku1088.herokuapp.com/unsubscribe.php?email=$email";
 
-		$mail->isSMTP();
-		$mail->SMTPAuth = true;
-		$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-		$mail->Host = "smtp.gmail.com";
-		$mail->Port = "587";
-		$mail->Username = "testmailassignmentphp@gmail.com";
-		$mail->Password = "Hritik@123!";
-		$mail->setFrom("testmailassignmentphp@gmail.com");
-		$mail->addAddress("hritikmiddha10@gmail.com");
-		$mail->isHTML(true);
-		$mail->Subject = "Mailer To Go Test";
-		$mail->Body = '
-  	          <p>Hello XKCDian</p>
-  	          Here is your new comic.
-  	          <h3>' . $comic['safe_title'] . "</h3>
-  	          <img src='" . $comic[' img'] . "' alt='some comic hehe'/>
-			<br />
-			To read the comic,  --> <a target='_blank' href='https://xkcd.com/" . $comic[' num'] . "'>Click here</a><br /> 
-			To Unsubscribe the Xkcd,  --> <a target='_blank' href='" .$urlun."'>Click here</a><br />";
-		$mail->addStringAttachment(file_get_contents($url), "$subject");
-		if ($mail->send()) {
-			echo '<br> <br><br> <div class="alert container alert-success alert-dismissible fade show" role="alert">
-				<strong>Email verification sent!!!</strong>  Please verify your email address.
-			  </div>';
-		} else {
-			echo "{$mail->ErrorInfo}";
-		}
-		header("Location: success.php");
+	$email = $_SESSION['email'];
+	$rand_comic = rand(0, 1000);
+	$url    = 'http://xkcd.com/' . $rand_comic . '/info.0.json';
+	$comic = json_decode(file_get_contents($url), true);
+	$title = 'Your New Comic : ' . $comic['safe_title'];
+	$name = $response_data->title;
+	$img = $response_data->img;
+	echo $img;
+	echo $name;
 	?>
 
 
