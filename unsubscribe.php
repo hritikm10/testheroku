53cccd356309fbd59c845415ec65975c7bf9551a
hritik <hritikmiddha10@gmail.com>
2022-01-22 12:50:36 +0530

    werdfg

--- unsubscribe.php
+++ unsubscribe.php
@@ -2,17 +2,17 @@
 session_start();
 $email = $_SESSION['email'];
 $emailV = $_GET['email'];
-if (isset($emailV)) {
-    if ($email == $emailV) {
-        include '_dbConnect.php';
-        $sql = "UPDATE `users` SET `active` = '0' WHERE `users`.`email` = '$email'";
-        $sqldel = "DELETE FROM `users` WHERE `users`.`email` = '$email'";
-        $result = mysqli_query($conn, $sql);
-        $resultDel = mysqli_query($conn, $sqldel);
-        session_unset();
-        session_destroy();
-        if ($result) {
-            echo ' <div class="container1">
+
+if ($email == $emailV) {
+    include '_dbConnect.php';
+    $sql = "UPDATE `users` SET `active` = '0' WHERE `users`.`email` = '$email'";
+    $sqldel = "DELETE FROM `users` WHERE `users`.`email` = '$email'";
+    $result = mysqli_query($conn, $sql);
+    $resultDel = mysqli_query($conn, $sqldel);
+    session_unset();
+    session_destroy();
+    if ($result) {
+        echo ' <div class="container1">
             <div class="brand-title">Unsubscribed</div>
             <br> <br> <br>
             <p>you have been successfully Unsubscribed to XKCD</p>
@@ -21,9 +21,9 @@ if (isset($emailV)) {
                 <button type="submit" class="btn btn-primary"><a style="color: white; text-decoration: none;" href="<?php echo "https://testheroku1088.herokuapp.com/index.php" ?>">Subscribe</a></button>
             </div>
         </div>';
-        }
-    } else {
-        echo ' <div class="container1">
+    }
+} else {
+    echo ' <div class="container1">
         <div class="brand-title" style="color: red;">Error!!!</div>
         <br> <br> <br>
         <p>Something Went Wrong!!!</p>
@@ -32,8 +32,7 @@ if (isset($emailV)) {
             <button type="submit" class="btn btn-primary"><a style="color: white; text-decoration: none;" href="<?php echo "https://testheroku1088.herokuapp.com/index.php" ?>">Subscribe</a></button>
         </div>
     </div>';
-        header("Location : index.php");
-    }
+    header("Location : index.php");
 }
 
 ?>
@@ -41,7 +40,7 @@ if (isset($emailV)) {
 <html lang="en">
 
 <head>
-<meta charset="utf-8">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="index.css">
     <script>
